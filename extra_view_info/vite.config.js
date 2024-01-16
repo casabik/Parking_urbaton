4670a8e8a8f53a0ee07fc96f71d1f6ec9393864d
Igor Tsarev <igorcarev146@gmail.com>
2024-01-17 01:25:21 +0300

    ex

--- extra_view_info/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import suidPlugin from "@suid/vite-plugin";
-
-export default defineConfig({
-  plugins: [suidPlugin(), solid()],
-})
